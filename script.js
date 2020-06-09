75047df168888d21ad360a08ad2e1fb8f283acde
Jatin Gaurav <jatin.soni007@gmail.com>
2020-06-10 03:13:56 +0530

    Spread operator

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let name = "Jatin Gaurav Soni";
-for(const char of name){
-    console.log(char);
-}
+let example1 = [1,2,3,5,6];
+let example2 = [...example1];
+example2.push(true);
+console.log(example2);
