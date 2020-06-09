5ca12244129e6bffc0d5140dfc95e2f8d9258e3c
Jatin Gaurav <jatin.soni007@gmail.com>
2020-06-10 03:05:07 +0530

    for of loop; string

--- script.js
+++ script.js
@@ -1,7 +1,4 @@
-let incomes = [100000,200000,300000];
-let total = 0;
-
-for(const income of incomes){
-    total += income;
+let name = "Jatin Gaurav Soni";
+for(const char of name){
+    console.log(char);
 }
-console.log(total);
