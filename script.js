368224ddabbd96eb460a3c1ed3431733cf84db73
Jatin Gaurav <jatin.soni007@gmail.com>
2020-06-15 06:46:04 +0530

    Async and await

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
-function add(param1,){
-    const example = {
-        name: 'Jatin',
-    }
-    console.log(example);
-} 
-add('heo');
+const appUrl = 'https://gstsuvidhakendra.org.in/api/get_gsk.php?pincode=201301';
+
+async function getGsk(){
+    const response = await fetch(appUrl);
+    const json = await response.json();
+    console.log(json[0]);
+}
+
+
+
+// function getGsk(){
+//     fetch(appUrl)
+//     .then((r)=>r.json())
+//     .then((json)=>{
+//         console.log(json[0].billing_company);
+//     }).catch((error)=>{
+//         console.log("Failed");
+//     });
+// };
+getGsk();
