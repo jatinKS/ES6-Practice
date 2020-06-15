328fca43d6262b46a9cb932a9a2a6fb9baa55d65
Jatin Gaurav <jatin.soni007@gmail.com>
2020-06-15 06:15:36 +0530

    Tralling commas

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
-import { Animal,Cat } from './animal.js';
-let mouse = new Animal('cat',4);
-//mouse.makeNoise();
-//console.log(Animal.return10());
-//console.log(mouse.metaData);
-let cat = new Cat();
-//cat.makeNoise();
-console.log(cat.metaData)
+function add(param1,){
+    const example = {
+        name: 'Jatin',
+    }
+    console.log(example);
+} 
+add('heo');
